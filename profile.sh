faccdebdb90f1ab0f24ea6e1fb715730d22083db
Rachel Dowavic <rachel.dowavic@gmail.com>
2018-12-10 16:48:05 +1100

    argument is now the name without the .dl extension so i can run gen_facts

--- profile.sh
+++ profile.sh
@@ -2,9 +2,12 @@
 ##### Then I'll just give you a flat profile. It is really simple my man.
 SOUFFLE="/home/rdow6615/souffle/src/souffle"
 program=$1
-##################
+#################
+# 0. GENERATE THE FACTS
+$program/gen_facts.sh > /dev/ null
+
 # 1. compile the .dl file
-CPPFLAGS="-pg" $SOUFFLE $program -F facts -w -o output --output-dir=/tmp/tmp.blah
+CPPFLAGS="-pg" $SOUFFLE $program/$program.dl -F facts -w -o output --output-dir=/tmp/tmp.blah
 # 2. run it and suppress the output
 echo "Running the program to generate runtime statistics..."
 ./output > /dev/null
